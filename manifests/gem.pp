bd3bf5add7833f27a46b633f54723b3d6dc10146
Brandon R. Stoner <bstoner@yrsi.com>
2011-07-28 20:43:09 +0000

    Added definition for installing packages via gem.

--- manifests/gem.pp
+++ manifests/gem.pp
@@ -1,6 +1,15 @@
 class ruby::gem {
+	define install($package_name) {
+		exec {
+			"ruby::gem::install::${name}":
+				command => "gem install ${package_name}",
+				path => ["/sbin/", "/bin", "/usr/bin", "/usr/local/bin"],
+		}
+	}
+
 	package {
 		"rubygems":
 			ensure => installed,
 	}
 }
+
